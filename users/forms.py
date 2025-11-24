066b29eb18fe8c1f0e7bfb6c68f5aa64b71646b3
vova <sidorookvolodia@gmail.com>
2025-11-24 23:20:16 +0200

    v002

--- users/forms.py
+++ users/forms.py
@@ -1,16 +1,49 @@
-# users/forms.py (новий файл)
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True, label="Email")  # Робимо email обов'язковим
+    email = forms.EmailField(required=True, label="Електронна пошта")
 
     class Meta:
         model = User
-        # Вказуємо, які поля будуть при реєстрації
         fields = ['username', 'email', 'first_name', 'last_name', 'phone_number']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # --- 1. Стилізація (Bootstrap) ---
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+
+        # --- 2. Переклад назв полів (Labels) ---
+        self.fields['username'].label = "Логін (Нікнейм)"
+        self.fields['first_name'].label = "Ім'я"
+        self.fields['last_name'].label = "Прізвище"
+        self.fields['phone_number'].label = "Номер телефону"
+
+        # Переклад полів пароля
+        if 'password1' in self.fields:
+            self.fields['password1'].label = "Пароль"
+        if 'password2' in self.fields:
+            self.fields['password2'].label = "Підтвердження пароля"
+
+        # --- 3. Переклад підказок (Help Text) ---
+
+        # Підказка для логіна
+        self.fields['username'].help_text = (
+            "Обов'язкове поле. Не більше 150 символів. "
+            "Тільки літери, цифри та символи @/./+/-/_."
+        )
+
+        # Підказка для пароля
+        # Ми замінюємо стандартний список вимог на зрозумілий текст
+        if 'password1' in self.fields:
+            self.fields['password1'].help_text = (
+                "Пароль має містити щонайменше 8 символів. "
+                "Він не може бути занадто простим або складатися лише з цифр."
+            )
 
 
 class UserUpdateForm(forms.ModelForm):
